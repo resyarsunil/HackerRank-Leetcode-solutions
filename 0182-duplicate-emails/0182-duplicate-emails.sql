484103ddd34393b5bd135208cdfcd8950458584b
Resya R Sunil <resya27085586@gmail.com>
2026-08-08 14:24:47 +0530

    4

---
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+select  email from Person
+group by email
+having count(email)>1;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
